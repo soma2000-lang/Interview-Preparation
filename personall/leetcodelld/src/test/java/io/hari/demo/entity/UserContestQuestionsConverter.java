0b6ed6d504b35b4b74493f7448673f9aecc1d244
Somasree Majumder <bishnupadamajumder32@gmail.com>
2024-05-14 06:34:38 +0530

    Adding leetcode lld

---
+++ personall/leetcodelld/src/test/java/io/hari/demo/entity/UserContestQuestionsConverter.java
@@ -0,0 +1,5 @@
+package test.java.io.hari.demo.entity;
+
+public record UserContestQuestionsConverter() {
+
+}
---
+++ personall/leetcodelld/src/test/java/io/hari/demo/java/io/hari/demo/JpaRepository.java
@@ -0,0 +1,5 @@
+package test.java.io.hari.demo.java.io.hari.demo;
+
+public interface JpaRepository<T1, T2> {
+
+}
